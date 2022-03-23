2292b9035918f7156bba76a3b3b37807a9c344ba
Tene <tene@nextunicorn.kr>
2022-03-24 00:29:36 +0900

    🔬Chore: CDD를 진행하기 위한 준비 #7

---
+++ components/Yoward.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Yoward from '.';
+
+export default {
+  title: 'Components/Yoward',
+  component: Yoward,
+};
+
+export const Default = () => <Yoward />;
---
+++ components/Yoward.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './Yoward.styled';
+
+const Yoward = () => {
+  return <Styled.Title>Yoward</Styled.Title>;
+};
+
+export default Yoward;
